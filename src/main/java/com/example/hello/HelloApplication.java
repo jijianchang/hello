3bda117fca81c04a6a163211dd15d046e3231af0
jijianchang <2235516049@qq.com>
2021-01-07 15:42:31 +0800

    test

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -9,7 +9,7 @@ public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
         System.out.println("hello");
-
+        System.out.println("ww");
 
     }
 
